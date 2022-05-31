3abbf537ac910f100c0f1dd52213dfddc06f3331
jubhavsar <juhi_bhavsar@yahoo.com>
2022-05-31 12:13:35 -0400

    import all image component manually

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Image from "next/image"
-import cat1 from "../public/images/cat1.gif"
-import cat2 from "../public/images/cat2.gif"
-import cat3 from "../public/images/cat3.gif"
+import cat1 from "../public/images/cat-1.gif"
+import cat2 from "../public/images/cat-2.gif"
+import cat3 from "../public/images/cat-3.gif"
 
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center w-full h-screen'>
       <h1 className='text-3xl'>The Cat App</h1>
-      <div className='relative w-32 h-32 overflow-hidden rounded-full ring-2 ring-pink-300 ring-offset-4'>
+      <div className='relative w-32 h-32 m-4 overflow-hidden rounded-full ring-2 ring-pink-300 ring-offset-4'>
         <Image
           objectFit='cover'
           src={cat1}
@@ -15,7 +15,25 @@ export default function Home() {
           layout='fill'
           priority
         />
-      </div>
+      </div> 
+      <div className='relative w-32 h-32 m-4 overflow-hidden rounded-full ring-2 ring-pink-300 ring-offset-4'>
+        <Image
+          objectFit='cover'
+          src={cat2}
+          alt='Picture of Cat1'
+          layout='fill'
+          priority
+        />
+      </div> 
+      <div className='relative w-32 h-32 m-4 overflow-hidden rounded-full ring-2 ring-pink-300 ring-offset-4'>
+        <Image
+          objectFit='cover'
+          src={cat3}
+          alt='Picture of Cat1'
+          layout='fill'
+          priority
+        />
+      </div>  
     </div>
   )
 }
