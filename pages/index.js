b64daf0f05082cdf8f4759383ad321f4feef836d
jubhavsar <juhi_bhavsar@yahoo.com>
2022-05-31 10:59:47 -0400

    imported images and image

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
+import Image from "next/image"
+import cat1 from "../public/images/cat1.gif"
+import cat2 from "../public/images/cat2.gif"
+import cat3 from "../public/images/cat3.gif"
+
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center w-full h-screen'>
